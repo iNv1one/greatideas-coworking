25a90ebb4fa7e05a9280b7ccf50f45f51b069817
iNv1one <Invictus6022@yandex.ru>
2025-10-06 19:34:17 +0300

    Fix migration dependencies - use correct migration number and dependency

--- startup_game/migrations/0002_completedevent.py
+++ startup_game/migrations/0002_completedevent.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('startup_game', '0002_addongroup_addon_menuitemvariant'),
+        ('startup_game', '0001_initial'),
     ]
 
     operations = [
